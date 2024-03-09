2c98bd10c964646f269e3e2a5192ca3905759cfb
Leduan_delbasto <123208423+Blockblocked@users.noreply.github.com>
2024-03-09 10:35:59 -0500

    cambios generales

--- Main/Productos/views.py
+++ Main/Productos/views.py
@@ -2,28 +2,26 @@ from django.shortcuts import get_object_or_404, render, redirect
 from .models import SliderImage
 from .models import Carrito, Producto, DetalleCarrito, Categoria
 
-# def home(request):
-#     imagen = SliderImage.objects.all()
-#     producto =  Producto.objects.all()
-#     categoria = Categoria.objects.all()
-    
-#     return render(request, 'principal/home.html', {'imagen': imagen, 'producto': producto, 'categoria' : categoria})
+
 
 def home(request):
     imagen = SliderImage.objects.all()
     # Esto solo carga los productos que esten marcados como especiales, en este caso carga al menos 10 como maximo
     productos_especiales = Producto.objects.filter(especial=True)[:10]
     categoria = Categoria.objects.all()
+  
     
     return render(request, 'principal/home.html', {'imagen': imagen, 'productos': productos_especiales, 'categoria': categoria})
 
 
+
 def shop(request):
     imagen = SliderImage.objects.all()
-    producto =  Producto.objects.all()
-    categoria = Categoria.objects.all()
+    categorias = Categoria.objects.all()
+    productos = Producto.objects.all()
 
-    return render(request, 'principal/shop.html')
+    return render(request, 'principal/shop.html', {'imagen': imagen, 'categorias': categorias, 'productos': productos})
+
 
 
 
