6aca57f513148be02586f2b45bd3c4d079c8d330
Leduan_delbasto <123208423+Blockblocked@users.noreply.github.com>
2024-03-22 08:24:24 -0500

    diseño mejorar

--- Main/Productos/static/js/navbar.js
+++ Main/Productos/static/js/navbar.js
@@ -1,54 +1,101 @@
-function agregarProductoAlCarrito(productoId) {
-    fetch(`/carrito/agregar/${productoId}/`, {
-        method: 'POST',
-        headers: {
-            'X-CSRFToken': obtenerCSRFToken(),
-            'Content-Type': 'application/json'
-        },
-        credentials: 'same-origin'
-    })
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            return response.json().then(data => {
-                // Muestra el mensaje de error en una alerta
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error',
-                    text: data.mensaje,
-                    timer: 2000, // Auto-cerrar después de 2 segundos
-                    timerProgressBar: true,
-                    showConfirmButton: false
-                });
-                throw new Error(data.mensaje); // Lanza un error para capturarlo en el catch
-            });
-        }
-    })
-    .then(data => {
-        // Llamar a la función para actualizar el número de productos en el carrito
-        actualizarNumeroProductosCarrito();
-        
-        // Muestra el mensaje de éxito en una alerta
-        Swal.fire({
-            icon: 'success',
-            title: 'Éxito',
-            text: data.mensaje,
-            timer: 2000, // Auto-cerrar después de 2 segundos
-            timerProgressBar: true,
-            showConfirmButton: false
-        });
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        // Muestra el mensaje de error en una alerta
-        Swal.fire({
-            icon: 'error',
-            title: 'Error',
-            text: error.message, // Accede al mensaje de error a través de error.message
-            timer: 2000, // Auto-cerrar después de 2 segundos
-            timerProgressBar: true,
-            showConfirmButton: false
-        });
+document.addEventListener('DOMContentLoaded', function() {
+    const botonesAgregarCarrito = document.querySelectorAll('.agregar-al-carrito');
+    botonesAgregarCarrito.forEach(boton => {
+    boton.addEventListener('click', function() {
+        const productoId = boton.dataset.productoId;
+        agregarProductoAlCarrito(productoId);
     });
-}
+    });
+    });
+    
+    function agregarProductoAlCarrito(productoId) {
+        fetch(`/carrito/agregar/${productoId}/`, {
+            method: 'POST',
+            headers: {
+                'X-CSRFToken': obtenerCSRFToken(),
+                'Content-Type': 'application/json'
+            },
+            credentials: 'same-origin'
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return response.json().then(data => {
+                    // Muestra el mensaje de error en una alerta
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error',
+                        text: data.mensaje,
+                        timer: 2000, // Auto-cerrar después de 2 segundos
+                        timerProgressBar: true,
+                        showConfirmButton: false
+                    });
+                });
+            }
+        })
+        .then(data => {
+            // Llamar a la función para actualizar el número de productos en el carrito
+            actualizarNumeroProductosCarrito();
+            
+            // Muestra el mensaje de éxito en una alerta
+            Swal.fire({
+                icon: 'success',
+                title: 'Éxito',
+                text: data.mensaje,
+                timer: 2000, // Auto-cerrar después de 2 segundos
+                timerProgressBar: true,
+                showConfirmButton: false
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // Muestra el mensaje de error en una alerta
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: data.mensaje,
+                timer: 2000, // Auto-cerrar después de 2 segundos
+                timerProgressBar: true,
+                showConfirmButton: false
+            });
+        });
+    }
+    
+    
+    function obtenerCSRFToken() {
+    const cookieValue = document.cookie.match(/csrftoken=([^ ;]+)/)[1];
+    return cookieValue;
+    }
+    
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+navToggle.addEventListener('click', () =>{
+   navMenu.classList.add('show-menu')
+})
+
+/* Menu hidden */
+navClose.addEventListener('click', () =>{
+   navMenu.classList.remove('show-menu')
+})
+
+/*=============== SEARCH ===============*/
+const search = document.getElementById('search'),
+      searchBtn = document.getElementById('search-btn'),
+      searchClose = document.getElementById('search-close')
+
+/* Search show */
+searchBtn.addEventListener('click', () =>{
+   search.classList.add('show-search')
+})
+
+/* Search hidden */
+searchClose.addEventListener('click', () =>{
+   search.classList.remove('show-search')
+})
+
